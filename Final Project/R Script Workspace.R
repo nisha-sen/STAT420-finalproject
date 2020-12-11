63c92824297d948258031c0f5841b29ca2e46505
Nisha Sen <nishams2@illinois.edu>
2020-12-11 11:51:36 -0800

    add functions to check assumptions

--- Final Project/R Script Workspace.R
+++ Final Project/R Script Workspace.R
@@ -1,4 +1,4 @@
-skaters_full = read.csv("Documents/Stat 420/Final Project/skater_stats.csv")
+skaters_full = read.csv("Documents/Stat 420/STAT420-finalproject/Final Project/skater_stats.csv")
 head(skaters_full)
 names(skaters_full)
 tail(skaters_full)
@@ -26,6 +26,7 @@ View(skaters$SHA)
 #ATOI is in colon time form
 
 str(skaters)
+skaters$Pos <- as.factor(skaters$Pos)
 skaters$Age <- as.numeric(skaters$Age)
 skaters$GP <- as.numeric(skaters$GP)
 skaters$G <- as.numeric(skaters$G)
@@ -52,56 +53,100 @@ skaters$FO. <- as.numeric(skaters$FO.)
 typeof(skaters$Pos)
 str(skaters)
 
-skaters$G[is.na(skaters$G)] <- 0
-skaters$A[is.na(skaters$A)] <- 0
-skaters$EVG[is.na(skaters$EVG)] <- 0
-skaters$PPG[is.na(skaters$PPG)] <- 0
-skaters$PTS[is.na(skaters$PTS)] <- 0
-skaters$SHG[is.na(skaters$SHG)] <- 0
-skaters$GWG[is.na(skaters$GWG)] <- 0
-skaters$EVA[is.na(skaters$EVA)] <- 0
-skaters$SHG[is.na(skaters$SHG)] <- 0
-skaters$PPA[is.na(skaters$PPA)] <- 0
-skaters$SHA[is.na(skaters$SHA)] <- 0
-skaters$S[is.na(skaters$S)] <- 0
-skaters$S.[is.na(skaters$S.)] <- 0
-skaters$TOI[is.na(skaters$TOI)] <- 0
-skaters$FO.[is.na(skaters$FO.)] <- 0
-skaters$X..[is.na(skaters$X..)] <- 0
-skaters$PIM[is.na(skaters$PIM)] <- 0
+skaters[is.na(skaters)] = 0
+
+# skaters$G[is.na(skaters$G)] <- 0
+# skaters$A[is.na(skaters$A)] <- 0
+# skaters$EVG[is.na(skaters$EVG)] <- 0
+# skaters$PPG[is.na(skaters$PPG)] <- 0
+# skaters$PTS[is.na(skaters$PTS)] <- 0
+# skaters$SHG[is.na(skaters$SHG)] <- 0
+# skaters$GWG[is.na(skaters$GWG)] <- 0
+# skaters$EVA[is.na(skaters$EVA)] <- 0
+# skaters$SHG[is.na(skaters$SHG)] <- 0
+# skaters$PPA[is.na(skaters$PPA)] <- 0
+# skaters$SHA[is.na(skaters$SHA)] <- 0
+# skaters$S[is.na(skaters$S)] <- 0
+# skaters$S.[is.na(skaters$S.)] <- 0
+# skaters$TOI[is.na(skaters$TOI)] <- 0
+# skaters$FO.[is.na(skaters$FO.)] <- 0
+# skaters$X..[is.na(skaters$X..)] <- 0
+# skaters$PIM[is.na(skaters$PIM)] <- 0
 str(skaters)
 #all NAs have been changed to 0s
+
+#Remove variables that are calculated using other variables in the data
+skaters_cleaned <- skaters[, c(4, 6, 7, 8, 10, 13, 21, 25, 26, 27, 28)]
+View(skaters_cleaned)
+names(skaters_cleaned)
+
+#full additive model
+goals_addfull = lm(G ~ ., data = skaters_cleaned)
+summary(goals_addfull)$r.sq
+
+#interactive model
+#goals_int = lm(G ~ Age * Pos * GP * A * PIM, data = skaters_cleaned)
+#summary(goals_int)
 
 #Response variable: Goals (G)
 #Some Key Variables: Age, Team (Tm), Position (Pos), Games Played (GP), Goals (G), Points (PTS)
 #don't use points; points is calculated w/ goals
 #don't use: PTS, GPG, EVG, PPG, SHG, GWG, S., X
 #additive models
-goals_addfull = lm(G ~ . - PTS - GPG - EVG - PPG - SHG - GWG - S. - X, data = skaters)
-summary(goals_addfull)$r.sq
-#R-sq for addfull model is 0.9244623
-goals_addmod = lm(G ~ . - X - Season - Player - Tm - Pos, data = skaters)
-summary(goals_addmod)$r.sq
-goals_mod1 = lm(G ~ Age + Pos, data = skaters)
-summary(goals_mod1)$r.sq
-goals_mod2 = lm(G ~ Age + Pos + GP, data = skaters)
-summary(goals_mod2)$r.sq
-goals_mod3 = lm(G ~ Age + Pos + GP + A, data = skaters)
-summary(goals_mod3)$r.sq
-goals_mod4 = lm(G ~ Age + Pos + GP + A + PIM, data = skaters)
-summary(goals_mod4)$r.sq
-goals_mod5 = lm(G ~ Age + Pos + GP + A + PIM + EVG, data = skaters)
-summary(goals_mod5)$r.sq
-goals_mod6 = lm(G ~ Age + Pos + GP + A + PIM + EVG + S, data = skaters)
-summary(goals_mod6)$r.sq
-goals_mod7 = lm(G ~ Age + Pos + GP + A + PIM + EVG + S + TOI, data = skaters)
-summary(goals_mod7)$r.sq
-goals_mod8 = lm(G ~ Age + Pos + GP + A + PIM + EVG + S + TOI + BLK, data = skaters)
-summary(goals_mod8)$r.sq
-#R-sq value of goals_mod8 = 0.9573586
+# goals_addfull = lm(G ~ . - PTS - GPG - EVG - PPG - SHG - GWG - S. - X, data = skaters)
+# summary(goals_addfull)$r.sq
+# #R-sq for addfull model is 0.9244623
+# goals_addmod = lm(G ~ . - X - Season - Player - Tm - Pos, data = skaters)
+# summary(goals_addmod)$r.sq
+# goals_mod1 = lm(G ~ Age + Pos, data = skaters)
+# summary(goals_mod1)$r.sq
+# goals_mod2 = lm(G ~ Age + Pos + GP, data = skaters)
+# summary(goals_mod2)$r.sq
+# goals_mod3 = lm(G ~ Age + Pos + GP + A, data = skaters)
+# summary(goals_mod3)$r.sq
+# goals_mod4 = lm(G ~ Age + Pos + GP + A + PIM, data = skaters)
+# summary(goals_mod4)$r.sq
+# goals_mod5 = lm(G ~ Age + Pos + GP + A + PIM + EVG, data = skaters)
+# summary(goals_mod5)$r.sq
+# goals_mod6 = lm(G ~ Age + Pos + GP + A + PIM + EVG + S, data = skaters)
+# summary(goals_mod6)$r.sq
+# goals_mod7 = lm(G ~ Age + Pos + GP + A + PIM + EVG + S + TOI, data = skaters)
+# summary(goals_mod7)$r.sq
+# goals_mod8 = lm(G ~ Age + Pos + GP + A + PIM + EVG + S + TOI + BLK, data = skaters)
+# summary(goals_mod8)$r.sq
+# #R-sq value of goals_mod8 = 0.9573586
 
 #interaction models take too long to run
 #don't use: PTS, GPG, EVG, PPG, SHG, GWG, S., X
 #goals_intfull = lm(G ~ Season * Player , data = skaters)
 #* Age * Tm * Pos * GP * A * X... * PIM * EVA * PPA * SHA * S * TOI * ATOI
 #* BLK * HIT * FOwin * FOloss * FO.
+
+#functions to check assumptions:
+plot_fitted_resid = function(model, pointcol = "dodgerblue", linecol = "darkorange") {
+  plot(fitted(model), resid(model), 
+       col = pointcol, pch = 20, cex = 1.5,
+       xlab = "Fitted", ylab = "Residuals")
+  abline(h = 0, col = linecol, lwd = 2)
+}
+
+plot_qq = function(model, pointcol = "dodgerblue", linecol = "darkorange") {
+  qqnorm(resid(model), col = pointcol, pch = 20, cex = 1.5)
+  qqline(resid(model), col = linecol, lwd = 2)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
